eddd33851b0638a9930a0c7c15bcb041061bb0ac
dtrift <89174579997@mail.ru>
2020-01-02 21:13:29 +0500

    Refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,11 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: [:show, :edit, :update, :destroy]
-
   def index
     @questions = Question.all
   end
 
   def show; end
 
-  def new
-    @question = Question.new
-  end
+  def new; end
 
   def create
     @question = Question.new(question_params)
@@ -24,7 +20,7 @@ class QuestionsController < ApplicationController
   def edit; end
 
   def update
-    if @question.update(question_params)
+    if question.update(question_params)
       redirect_to @question
     else
       render :edit
@@ -32,15 +28,17 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question.destroy
+    question.destroy
     redirect_to questions_path
   end
 
   private
 
-  def find_question
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
+
+  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)

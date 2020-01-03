775eb6b751063f8875083b2360f3d98ee80edf4c
dtrift <89174579997@mail.ru>
2020-01-03 21:53:30 +0500

    Dicision complete

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question)}
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer in the DB' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end

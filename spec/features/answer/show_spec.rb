e7587348342746f0139d3201c596fde6ffb5e87b
dtrift <89174579997@mail.ru>
2020-01-09 22:11:50 +0500

    User can view the question and answers to it

---
+++ spec/features/answer/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User can view the question', %q{
+  The user can view the question 
+  and answers to it.
+} do
+
+  given!(:question) { create :question }
+  given!(:answers) { create_list(:q_answers, 2, question: question) }
+
+  scenario 'User view the question with answers' do
+    visit question_path(question)
+    
+    answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+end

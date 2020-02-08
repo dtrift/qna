10165e4a9350ff0b0e047aa38a909caf154ec108
dtrift <89174579997@mail.ru>
2020-02-08 19:17:50 +0500

    Refactoring

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -7,25 +7,35 @@ RSpec.describe AttachmentsController, type: :controller do
   let!(:answer) { create :answer, :add_file, question: question, user: author }
 
   describe 'DELETE #destroy' do
-    before { login author }
-
     context 'Authenticated Author' do
       it 'delete the attachment' do
+        login author
+
         expect { delete :destroy, params: { id: answer.files.first, question_id: question }, format: :js }
           .to change(ActiveStorage::Attachment, :count).by(-1)
       end
 
       it 'reder template destroy' do
+        login author
+        
         delete :destroy, params: { id: answer.files.first, question_id: question }, format: :js
+
         expect(response).to render_template :destroy
       end
     end
 
     context 'Authenticated Non author' do
-      let!(:second_answer) { create(:answer, :add_file, question: question, user: user) }
-
       it 'trying to delete another attachment' do
-        expect { delete :destroy, params: { id: second_answer.files.first, question_id: question }, format: :js }
+        login user
+
+        expect { delete :destroy, params: { id: answer.files.first, question_id: question }, format: :js }
+          .to_not change(ActiveStorage::Attachment, :count)
+      end
+    end
+
+    context 'Guest' do
+      it 'trying to delete attachment' do
+        expect { delete :destroy, params: { id: answer.files.first, question_id: question }, format: :js }
           .to_not change(ActiveStorage::Attachment, :count)
       end
     end

ea07cca69f7617efd60eb05270e3393bc12427df
dtrift <89174579997@mail.ru>
2020-02-11 16:25:07 +0500

    User can add multiple links to question/answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,8 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :links, dependent: :destroy, as: :linkable
-  accepts_nested_attributes_for :links, reject_if: :all_blank
+  accepts_nested_attributes_for :links, allow_destroy: true,
+                                        reject_if: :all_blank
 
   has_many_attached :files
 

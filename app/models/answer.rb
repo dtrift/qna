6ed680f8e4c12a465d9644bb7c2e0c1d190b070f
dtrift <89174579997@mail.ru>
2020-02-11 14:25:51 +0500

    Refactoring

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,10 +2,9 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :links, dependent: :destroy, as: :linkable
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
   validates :best, inclusion: [true, false]
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,10 +2,9 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, -> { order best: :desc, created_at: :desc }, dependent: :destroy
   has_many :links, dependent: :destroy, as: :linkable
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :title, :body, presence: true
 end

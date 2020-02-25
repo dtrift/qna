c1807a0570e53f30c3d3ec08ca1a057604bf96e7
dtrift <89174579997@mail.ru>
2020-02-25 17:32:09 +0500

    Lesson 12 Authorization in Rails

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -3,5 +3,5 @@ class Authorization < ApplicationRecord
 
   validates :provider, presence: true
   validates :uid, presence: true
-  validates_uniqueness_of :provider, scope: :uid 
+  # validates_uniqueness_of :provider, scope: :uid 
 end
--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe Authorization, type: :model do
   describe 'validations' do
     it { should validate_presence_of :provider }
     it { should validate_presence_of :uid }
-    it { should validate_uniqueness_of(:provider).scoped_to(:uid) }
+    # it { should validate_uniqueness_of(:provider).scoped_to(:uid) }
   end
 end

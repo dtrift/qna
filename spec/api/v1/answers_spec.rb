572f972611c59b9ce38d961a0e45b2619bc774c3
dtrift <89174579997@mail.ru>
2020-02-29 12:34:53 +0500

    Add API for answer

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -6,18 +6,19 @@ describe 'Answers API', type: :request do
     "ACCEPT" => "application/json"
   } }
 
-  describe 'GET /api/v1/questions/:qiuestion_id/answers' do
-    let(:user) { create :user }
-    let(:question) { create :question, user: user }
-    let!(:answers) { create_list :answer, 3, user: user, question: question }
+  let(:access_token) { create :access_token }
+  let(:user) { create :user }
+  let(:question) { create :question, user: user }
+  let!(:answers) { create_list :answer, 3, user: user, question: question }
+  let(:answer) { answers.first }
+
+  describe 'GET /api/v1/questions/:question_id/answers' do
     let(:method) { :get }
     let(:api_path) { "/api/v1/questions/#{question.id}/answers" }
 
     it_behaves_like 'API Authorizable'
 
     context 'authorized' do
-      let(:access_token) { create :access_token }
-      let(:answer) { answers.first }
       let(:answers_response) { json['answers'] }
       let(:answer_response) { answers_response.first }
 
@@ -29,6 +30,30 @@ describe 'Answers API', type: :request do
 
       it 'returns list of answers' do
         expect(answers_response.size).to eq 3
+      end
+
+      it 'returns all public fields' do
+        %w[id body created_at updated_at user_id].each do |attr|
+          expect(answer_response[attr]).to eq answer.send(attr).as_json
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/questions/:question_id/answers/:id' do
+    let(:method) { :get }
+    let(:api_path) { "/api/v1/questions/#{question.id}/answers/#{answer.id}" }
+
+    it_behaves_like 'API Authorizable'
+
+    context 'authorized' do
+      # let(:answers_response) { json['answers'] }
+      let(:answer_response) { json['answer'] }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns status 200' do
+        expect(response).to be_successful
       end
 
       it 'returns all public fields' do

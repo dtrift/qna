8057c08ecb28755a3171f4af0fdb9a176b503a59
dtrift <89174579997@mail.ru>
2020-02-29 14:45:01 +0500

    Add tests for spec/api/v1/answers_spec.rb

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -9,7 +9,7 @@ describe 'Answers API', type: :request do
   let(:access_token) { create :access_token }
   let(:user) { create :user }
   let(:question) { create :question, user: user }
-  let!(:answers) { create_list :answer, 3, user: user, question: question }
+  let!(:answers) { create_list :answer, 3, :add_file, user: user, question: question }
   let(:answer) { answers.first }
 
   describe 'GET /api/v1/questions/:question_id/answers' do
@@ -47,8 +47,11 @@ describe 'Answers API', type: :request do
     it_behaves_like 'API Authorizable'
 
     context 'authorized' do
-      # let(:answers_response) { json['answers'] }
       let(:answer_response) { json['answer'] }
+      let!(:comments) { create_list :comment, 3, user: user, commentable: answer }
+      let(:comment) { comments.first }
+      let(:comment_response) { answer_response['comments'].first }
+      let!(:links) { create_list :link, 4, linkable: answer }
 
       before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
@@ -60,6 +63,26 @@ describe 'Answers API', type: :request do
         %w[id body created_at updated_at user_id].each do |attr|
           expect(answer_response[attr]).to eq answer.send(attr).as_json
         end
+      end
+
+      context 'comments' do
+        it 'are returned all' do
+          expect(answer_response['comments'].size).to eq 3
+        end
+
+        it 'returns all public fields' do
+          %w[id content created_at updated_at user_id].each do |attr|
+            expect(comment_response[attr]).to eq comment.send(attr).as_json
+          end
+        end
+      end
+
+      it 'returns links' do
+        expect(answer_response['links'].size).to eq 4
+      end
+
+      it 'returns files' do
+        expect(answer_response['files'].first.size).to eq 1 
       end
     end
   end

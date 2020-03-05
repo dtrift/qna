ebffcb902a5a343e3d551b69c31bf088495724b0
dtrift <89174579997@mail.ru>
2020-03-05 22:31:49 +0500

    delete Fix

--- app/services/new_answer_digest_service.rb
+++ app/services/new_answer_digest_service.rb
@@ -1,8 +1,6 @@
 class NewAnswerDigestService
   def send_notification(answer)
-    subscribers = answer.question.includes(:subscribers)
-
-    subscribers.find_each do |user|
+    answer.question.subscribers.find_each do |user|
       NewAnswerDigestMailer.send_for(user, answer).deliver_later
     end
   end

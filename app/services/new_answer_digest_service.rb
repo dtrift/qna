9e8c16de11cd7b4912ab6291458085270be38e46
dtrift <89174579997@mail.ru>
2020-03-05 22:38:07 +0500

    Fix n+1

--- app/services/new_answer_digest_service.rb
+++ app/services/new_answer_digest_service.rb
@@ -1,6 +1,8 @@
 class NewAnswerDigestService
   def send_notification(answer)
-    answer.question.subscribers.find_each do |user|
+    users = User.joins(:subscriptions).where(subscriptions: { question_id: answer.question })
+
+    users.find_each do |user|
       NewAnswerDigestMailer.send_for(user, answer).deliver_later
     end
   end

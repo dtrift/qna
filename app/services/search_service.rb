a4ba5b6f223f758068b1ebd1f05be16d9d5366a9
dtrift <89174579997@mail.ru>
2020-03-11 22:30:06 +0500

    Refactoring ./services/search_service.rb

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -2,20 +2,10 @@ class SearchService
   RESOURCES = %w[All Question Answer Comment User].freeze
 
   def self.call(query, resource)
-    resource_valid?(resource) ? run_search(query, resource) : exception_invalid_resource
-  end
-
-  private
-
-  def self.resource_valid?(resource)
-    RESOURCES.include?(resource)
-  end
-
-  def self.run_search(query, resource)
-    resource == 'All' ? ThinkingSphinx.search(query) : resource.constantize.search(query)
-  end
-
-  def self.exception_invalid_resource
-    raise StandardError, 'Wrong resource! Select available resource.'
+    if RESOURCES.include?(resource)
+      resource == 'All' ? ThinkingSphinx.search(query) : resource.constantize.search(query)
+    else
+      raise StandardError, 'Wrong resource! Select available resource.'
+    end
   end
 end

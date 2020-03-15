45ade9ca4c56d9604afe334f8d749add6a27d10f
dtrift <89174579997@mail.ru>
2020-03-15 15:11:13 +0500

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_user, 'deployer'
 append :linked_files, "config/database.yml", "config/master.key", "config/credentials/production.key"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage""public/packs", ".bundle", "node_modules"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -39,3 +39,15 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# before "deploy:assets:precompile", "deploy:yarn_install"
+# namespace :deploy do
+#   desc "Run rake yarn install"
+#   task :yarn_install do
+#     on roles(:web) do
+#       within release_path do
+#         execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+#       end
+#     end
+#   end
+# end

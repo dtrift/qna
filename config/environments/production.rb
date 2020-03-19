f51f3ad4f9628c2e7dbd3cf5ad1442e2a7aac714
dtrift <89174579997@mail.ru>
2020-03-19 18:21:48 +0500

    Fix action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'http://qna.rfix.cc' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -75,7 +75,7 @@ Rails.application.configure do
     user_name:            Rails.application.credentials[:SMTP][:USERNAME],
     password:             Rails.application.credentials[:SMTP][:PASSWORD],
     authentication:       'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: false
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to

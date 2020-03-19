d7cb5aaa098eab6a72da4eb7d08d499854789dde
dtrift <89174579997@mail.ru>
2020-03-19 18:38:07 +0500

    Fix action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
+
   config.action_mailer.perform_caching = true
+
   config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.default_url_options = { host: 'http://qna.rfix.cc' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

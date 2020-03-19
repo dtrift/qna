65157cf145f4f6810820961c1e48d95f1003dac4
dtrift <89174579997@mail.ru>
2020-03-19 19:44:12 +0500

    Fix action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 2525,
     user_name:            Rails.application.credentials[:SMTP][:USERNAME],
     password:             Rails.application.credentials[:SMTP][:PASSWORD],
     authentication:       :login,
